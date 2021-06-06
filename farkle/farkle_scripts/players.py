a60aa1f3eed3f49b406ccef58b81f037853b7cef
jzanfes <84643489+jzanfes@users.noreply.github.com>
2021-06-06 00:10:51 -0400

    new player list creation

--- farkle/farkle_scripts/players.py
+++ farkle/farkle_scripts/players.py
@@ -1,3 +1,17 @@
 class PlayerMgmt:
-    def players_new_game:
+    def players_new_game():
         players = []
+        print('Please enter player names individually and then enter "q" when all players have been added.')
+        player_entry = True
+        while player_entry:
+            new_player = input('Player Name:').strip()
+            if new_player == 'q':
+                player_entry = False
+            else:
+                players.append(new_player.title().strip())
+                print(players)
+        return tuple(players)
+
+
+player_tuple = PlayerMgmt.players_new_game()
+print(player_tuple)
